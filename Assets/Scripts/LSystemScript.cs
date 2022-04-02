572c186428a87e5b1d8f9550c057a45e10dff682
nonamed <stirb.nicht.vor.mir@gmail.com>
2022-04-02 02:05:12 +0200

    L system test

--- Assets/Scripts/LSystemScript.cs
+++ Assets/Scripts/LSystemScript.cs
@@ -1,18 +1,91 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
+public class TransformInfo
+{
+    public Vector3 position;
+    public Quaternion rotation;
+}
 public class LSystemScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]private int iterations  = 4;
+    [SerializeField] private GameObject Branch;
+    [SerializeField] private float length =  10;
+    [SerializeField] private float angle =  30;
+
+    private const string axiom = "X";
+
+    private Stack<TransformInfo> transformStack;
+    private Dictionary<char, string> rules;
+
+    private string currentString = string.Empty;
     void Start()
     {
-        
+        transformStack = new Stack<TransformInfo>();
+        rules = new Dictionary<char, string>
+        {
+            {'X', "[F-[[X]+X]+F[+FX]-X]" },
+            {'F', "FF" }
+        };
+        Generate();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Generate()
     {
-        
+        currentString = axiom;
+        StringBuilder sb = new StringBuilder();
+
+        for(int i  = 0; i < iterations; i++)
+        {
+            foreach(char c in currentString)
+            {
+                sb.Append(rules.ContainsKey(c) ? rules[c] : c.ToString());
+            }
+            
+            currentString = sb.ToString();
+            sb = new StringBuilder();
+        }
+
+
+        foreach (char c in currentString)
+        {
+            switch(c)
+            {
+                case 'F':
+                    Vector3 initialPosition = transform.position;
+                    transform.Translate(Vector3.up * length);
+                    GameObject treeSegment = Instantiate(Branch);
+                    treeSegment.GetComponent<LineRenderer>().SetPosition(0, initialPosition);
+                    treeSegment.GetComponent<LineRenderer>().SetPosition(1, transform.position);
+                    break;
+                case 'X':
+                    break;
+                case '+':
+                    transform.Rotate(Vector3.back * angle);
+                    break;
+                case '-':
+                    transform.Rotate(Vector3.forward * angle);
+                    break;
+                case '[':
+                    transformStack.Push(new TransformInfo()
+                    {
+                        position =transform.position,
+                        rotation= transform.rotation
+                    });
+                    break;
+                case ']':
+                    TransformInfo ti = transformStack.Pop();
+                    transform.position = ti.position;
+                    transform.rotation = ti.rotation;
+                    break;
+
+                default:
+                    throw new InvalidOperationException("invalid L operation");
+            }
+        }
     }
+
+    
 }
--- readme.txt
+++ readme.txt
@@ -3,3 +3,5 @@ Before pushing to bitbucket I added 2 free assets:
 -  https://sketchfab.com/3d-models/desert-stone-ground-def353b3968947d496a51f3783cd6eaa
 - https://sketchfab.com/3d-models/post-apocalyptic-building-3085571b81d34c978f8a30582d0ba072
 and made a scene with these 3D assets using procedural ivy content from the original project.
+
+https://www.youtube.com/watch?v=tUbTGWl-qus tree  L system tutorial
