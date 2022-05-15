f8390b9e2589ae37572e80d43499d2e37f273a18
Maria Shchurova <stirb.nicht.vor.mir@gmail.com>
2022-05-15 03:19:55 +0200

    grass prefab modified

--- Assets/Scripts/GrassGrow.cs
+++ Assets/Scripts/GrassGrow.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class GrassGrow : MonoBehaviour
 {
+    const string AMOUNT = "_Amount";
+    public float currentAmount = -1;
+    public float growthSpeed = 0.5f;
+
+    Material material;
     // Start is called before the first frame update
     void Start()
     {
         transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+
+        material = GetComponent<MeshRenderer>().material;
+        material.SetFloat(AMOUNT, currentAmount);
+
     }
 
     // Update is called once per frame
@@ -16,6 +25,12 @@ public class GrassGrow : MonoBehaviour
         if(transform.localScale.x <= 1)
         {
             transform.localScale += new Vector3(0.01f * Time.deltaTime, 0.01f * Time.deltaTime, 0.01f * Time.deltaTime);
+        }
+
+        if(currentAmount < 1)
+        {
+            currentAmount += Time.deltaTime * growthSpeed;
+            material.SetFloat(AMOUNT, currentAmount);
         }
     }
 }
