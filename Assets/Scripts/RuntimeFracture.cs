4d27a8670768b3bf4d8a6c04682167387cde5b02
Boyan <tigara12@gmail.com>
2022-06-21 20:24:43 +0200

    Changed when fracturing is done to reduce lag

--- Assets/Scripts/RuntimeFracture.cs
+++ Assets/Scripts/RuntimeFracture.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Project.Scripts.Fractures
 {
@@ -12,30 +13,54 @@ namespace Project.Scripts.Fractures
         public float brokenPercentage;
         public float brokenObjects;
         float totalObjects;
+        int objectCounter;
+        bool doneFracturing;
 
         GameObject[] objectsToFracture; //size of this is number of breakable objects
+        Text fracturingText;
 
 
         private void Start()
         {
-            objectsToFracture = GameObject.FindGameObjectsWithTag("Fracture");
-            for (int i = 0; i < objectsToFracture.Length; i++)
-            {
-                FractureThis fracturing = objectsToFracture[i].AddComponent<FractureThis>();
-                fracturing.chunks = setChunks;
-                fracturing.density = setDensity;
-                fracturing.internalStrength = setInternalStrength;
-
-                MeshCollider mesh = objectsToFracture[i].AddComponent<MeshCollider>();
-                mesh.convex = true;
-                Rigidbody rigidbody = objectsToFracture[i].AddComponent<Rigidbody>();
-                rigidbody.isKinematic = true;
-            }
+            objectsToFracture = GameObject.FindGameObjectsWithTag("Fracture");           
             totalObjects = objectsToFracture.Length;
+            fracturingText = GameObject.Find("FracturingText").GetComponent<Text>();
             Debug.LogWarning("number of objects: " + objectsToFracture.Length);
         }
         private void Update()
         {
+            int objPerFrame = 0;
+            if(doneFracturing == false)
+            {
+                while (objectCounter < totalObjects && objPerFrame < 3)
+                {
+                    FractureThis fracturing = objectsToFracture[objectCounter].AddComponent<FractureThis>();
+                    fracturing.chunks = setChunks;
+                    fracturing.density = setDensity;
+                    fracturing.internalStrength = setInternalStrength;
+
+                    MeshCollider mesh = objectsToFracture[objectCounter].AddComponent<MeshCollider>();
+                    mesh.convex = true;
+                    Rigidbody rigidbody = objectsToFracture[objectCounter].AddComponent<Rigidbody>();
+                    rigidbody.isKinematic = true;
+
+                    fracturingText.text = "Fracturing Objects: " + objectCounter + " / " + totalObjects;
+                    objectCounter++;
+                    objPerFrame++;
+                    Debug.LogWarning("Fractured " + objectCounter + " objects");
+                }
+
+                
+
+                if (objectCounter >= totalObjects)
+                {
+                    fracturingText.transform.gameObject.SetActive(false);
+                    doneFracturing = true;
+                    Debug.LogWarning("Done Fracturing");
+                }
+                    
+            }
+
             brokenPercentage = (brokenObjects / totalObjects) * 100;
         }
     }
